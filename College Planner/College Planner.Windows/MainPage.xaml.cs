66c8a9755d56821e2838d02920bd56719ab21634
Marc Gatti <gattim@canisius.edu>
2015-01-16 01:57:14 -0500

    Populated the Calendar with Dates
    Calendar now recognizes the proper date and populates accordingly.
    The Calendar now only has dates of the month as visible.

--- College Planner/College Planner.Windows/MainPage.xaml.cs
+++ College Planner/College Planner.Windows/MainPage.xaml.cs
@@ -21,26 +21,28 @@ namespace College_Planner
     public sealed partial class MainPage : Page
     {
         private Rectangle selectedDay;
-        private Rectangle[,] rectDays = new Rectangle[5,7];
-        private TextBlock[,] txtDays = new TextBlock[5, 7];
 
         public MainPage()
         {
-            instantiateDays();
-
             int curDayOfMonth = DateTime.Now.Day;
             Rectangle curDay = new Rectangle();
 
             this.InitializeComponent();
-            populateMonth();
+            clearMonth();
+            populateMonth(datePicker.Date.Year, datePicker.Date.Month);
             selectDay(curDay);
-            txt0x0.Text = calcFirstDay(2015, 1).ToString();
         }
 
         private void rect_PointerPressed(object sender, PointerRoutedEventArgs e) {
             Rectangle newDay = sender as Rectangle;
             deselectDay();
             selectDay(newDay);
+        }
+
+        private void datePicker_DateChanged(object sender, DatePickerValueChangedEventArgs e) {
+            DatePicker day = sender as DatePicker;
+            clearMonth();
+            populateMonth(day.Date.Year, day.Date.Month);
         }
 
         private void selectDay(Rectangle actDay) {
@@ -53,19 +55,167 @@ namespace College_Planner
             selectedDay = null;
         }
 
-        private void populateMonth() {
-            int month = datePicker.Date.Month;
-            int year = datePicker.Date.Year;
+        private void populateMonth(int year, int month) {
             int daysInMonth = calcNumDaysInMonth(year, month);
             int firstWeekDayOfMonth = calcFirstDay(year, month);
 
-            for (int i = 0; i < daysInMonth; i++) {
-                for (int j = 0; j < 5; j++) {
-                    for (int k = (j == 0) ? firstWeekDayOfMonth : 0; k < 7; k++) {
-                        
-                    }
+            for (int i = 1; i <= daysInMonth; i++) {
+                int rectDay = i + firstWeekDayOfMonth;
+                switch (rectDay) {
+                    case 1:
+                        txt0x0.Text = i.ToString();
+                        rect0x0.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 2:
+                        txt0x1.Text = i.ToString();
+                        rect0x1.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 3:
+                        txt0x2.Text = i.ToString();
+                        rect0x2.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 4:
+                        txt0x3.Text = i.ToString();
+                        rect0x3.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 5:
+                        txt0x4.Text = i.ToString();
+                        rect0x4.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 6:
+                        txt0x5.Text = i.ToString();
+                        rect0x5.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 7:
+                        txt0x6.Text = i.ToString();
+                        rect0x6.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 8:
+                        txt1x0.Text = i.ToString();
+                        rect1x0.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 9:
+                        txt1x1.Text = i.ToString();
+                        rect1x1.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 10:
+                        txt1x2.Text = i.ToString();
+                        rect1x2.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 11:
+                        txt1x3.Text = i.ToString();
+                        rect1x3.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 12:
+                        txt1x4.Text = i.ToString();
+                        rect1x4.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 13:
+                        txt1x5.Text = i.ToString();
+                        rect1x5.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 14:
+                        txt1x6.Text = i.ToString();
+                        rect1x6.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 15:
+                        txt2x0.Text = i.ToString();
+                        rect2x0.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 16:
+                        txt2x1.Text = i.ToString();
+                        rect2x1.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 17:
+                        txt2x2.Text = i.ToString();
+                        rect2x2.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 18:
+                        txt2x3.Text = i.ToString();
+                        rect2x3.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 19:
+                        txt2x4.Text = i.ToString();
+                        rect2x4.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 20:
+                        txt2x5.Text = i.ToString();
+                        rect2x5.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 21:
+                        txt2x6.Text = i.ToString();
+                        rect2x6.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 22:
+                        txt3x0.Text = i.ToString();
+                        rect3x0.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 23:
+                        txt3x1.Text = i.ToString();
+                        rect3x1.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 24:
+                        txt3x2.Text = i.ToString();
+                        rect3x2.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 25:
+                        txt3x3.Text = i.ToString();
+                        rect3x3.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 26:
+                        txt3x4.Text = i.ToString();
+                        rect3x4.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 27:
+                        txt3x5.Text = i.ToString();
+                        rect3x5.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 28:
+                        txt3x6.Text = i.ToString();
+                        rect3x6.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 29:
+                        txt4x0.Text = i.ToString();
+                        rect4x0.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 30:
+                        txt4x1.Text = i.ToString();
+                        rect4x1.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 31:
+                        txt4x2.Text = i.ToString();
+                        rect4x2.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 32:
+                        txt4x3.Text = i.ToString();
+                        rect4x3.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 33:
+                        txt4x4.Text = i.ToString();
+                        rect4x4.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 34:
+                        txt4x5.Text = i.ToString();
+                        rect4x5.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 35:
+                        txt4x6.Text = i.ToString();
+                        rect4x6.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 36:
+                        txt5x0.Text = i.ToString();
+                        rect5x0.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 37:
+                        txt5x1.Text = i.ToString();
+                        rect5x1.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
+                    case 38:
+                        txt5x2.Text = i.ToString();
+                        rect5x2.Fill = new SolidColorBrush(Colors.WhiteSmoke);
+                        break;
                 }
-            }
+            }      
         }
 
         private int calcNumDaysInMonth(int year, int month) {
@@ -78,28 +228,53 @@ namespace College_Planner
             return (int)new DateTime(year, month, 1).DayOfWeek;
         }
 
-        private void instantiateDays() {
-            rectDays[0, 0] = rect0x0; rectDays[0, 1] = rect0x1; rectDays[0, 2] = rect0x2; rectDays[0, 3] = rect0x3;
-            rectDays[0, 4] = rect0x4; rectDays[0, 5] = rect0x5; rectDays[0, 6] = rect0x6;
-            rectDays[1, 0] = rect1x0; rectDays[1, 1] = rect1x1; rectDays[1, 2] = rect1x2; rectDays[1, 3] = rect1x3;
-            rectDays[1, 4] = rect1x4; rectDays[1, 5] = rect1x5; rectDays[1, 6] = rect1x6;
-            rectDays[2, 0] = rect2x0; rectDays[2, 1] = rect2x1; rectDays[2, 2] = rect2x2; rectDays[2, 3] = rect2x3;
-            rectDays[2, 4] = rect2x4; rectDays[2, 5] = rect2x5; rectDays[2, 6] = rect2x6;
-            rectDays[3, 0] = rect3x0; rectDays[3, 1] = rect3x1; rectDays[3, 2] = rect3x2; rectDays[3, 3] = rect3x3;
-            rectDays[3, 4] = rect3x4; rectDays[3, 5] = rect3x5; rectDays[3, 6] = rect3x6;
-            rectDays[4, 0] = rect4x0; rectDays[4, 1] = rect4x1; rectDays[4, 2] = rect4x2; rectDays[4, 3] = rect4x3;
-            rectDays[4, 4] = rect4x4; rectDays[4, 5] = rect4x5; rectDays[4, 6] = rect4x6;
+        private void clearMonth() {
+            changeDayColors(Colors.DarkBlue);
+        }
 
-            txtDays[0, 0] = txt0x0; txtDays[0, 1] = txt0x1; txtDays[0, 2] = txt0x2; txtDays[0, 3] = txt0x3;
-            txtDays[0, 4] = txt0x4; txtDays[0, 5] = txt0x5; txtDays[0, 6] = txt0x6;
-            txtDays[1, 0] = txt1x0; txtDays[1, 1] = txt1x1; txtDays[1, 2] = txt1x2; txtDays[1, 3] = txt1x3;
-            txtDays[1, 4] = txt1x4; txtDays[1, 5] = txt1x5; txtDays[1, 6] = txt1x6;
-            txtDays[2, 0] = txt2x0; txtDays[2, 1] = txt2x1; txtDays[2, 2] = txt2x2; txtDays[2, 3] = txt2x3;
-            txtDays[2, 4] = txt2x4; txtDays[2, 5] = txt2x5; txtDays[2, 6] = txt2x6;
-            txtDays[3, 0] = txt3x0; txtDays[3, 1] = txt3x1; txtDays[3, 2] = txt3x2; txtDays[3, 3] = txt3x3;
-            txtDays[3, 4] = txt3x4; txtDays[3, 5] = txt3x5; txtDays[3, 6] = txt3x6;
-            txtDays[4, 0] = txt4x0; txtDays[4, 1] = txt4x1; txtDays[4, 2] = txt4x2; txtDays[4, 3] = txt4x3;
-            txtDays[4, 4] = txt4x4; txtDays[4, 5] = txt4x5; txtDays[4, 6] = txt4x6;
+        private void changeDayColors(Color c) {
+            rect0x0.Fill = new SolidColorBrush(c);
+            rect0x1.Fill = new SolidColorBrush(c);
+            rect0x2.Fill = new SolidColorBrush(c);
+            rect0x3.Fill = new SolidColorBrush(c);
+            rect0x4.Fill = new SolidColorBrush(c);
+            rect0x5.Fill = new SolidColorBrush(c);
+            rect0x6.Fill = new SolidColorBrush(c);
+            rect0x0.Fill = new SolidColorBrush(c);
+            rect0x1.Fill = new SolidColorBrush(c);
+            rect0x2.Fill = new SolidColorBrush(c);
+            rect0x3.Fill = new SolidColorBrush(c);
+            rect0x4.Fill = new SolidColorBrush(c);
+            rect0x5.Fill = new SolidColorBrush(c);
+            rect0x6.Fill = new SolidColorBrush(c);
+            rect1x0.Fill = new SolidColorBrush(c);
+            rect1x1.Fill = new SolidColorBrush(c);
+            rect1x2.Fill = new SolidColorBrush(c);
+            rect1x3.Fill = new SolidColorBrush(c);
+            rect1x4.Fill = new SolidColorBrush(c);
+            rect1x5.Fill = new SolidColorBrush(c);
+            rect1x6.Fill = new SolidColorBrush(c);
+            rect2x0.Fill = new SolidColorBrush(c);
+            rect2x1.Fill = new SolidColorBrush(c);
+            rect2x2.Fill = new SolidColorBrush(c);
+            rect2x3.Fill = new SolidColorBrush(c);
+            rect2x4.Fill = new SolidColorBrush(c);
+            rect2x5.Fill = new SolidColorBrush(c);
+            rect2x6.Fill = new SolidColorBrush(c);
+            rect4x0.Fill = new SolidColorBrush(c);
+            rect4x1.Fill = new SolidColorBrush(c);
+            rect4x2.Fill = new SolidColorBrush(c);
+            rect4x3.Fill = new SolidColorBrush(c);
+            rect4x4.Fill = new SolidColorBrush(c);
+            rect4x5.Fill = new SolidColorBrush(c);
+            rect4x6.Fill = new SolidColorBrush(c);
+            rect5x0.Fill = new SolidColorBrush(c);
+            rect5x1.Fill = new SolidColorBrush(c);
+            rect5x2.Fill = new SolidColorBrush(c);
+            rect5x3.Fill = new SolidColorBrush(c);
+            rect5x4.Fill = new SolidColorBrush(c);
+            rect5x5.Fill = new SolidColorBrush(c);
+            rect5x6.Fill = new SolidColorBrush(c);
         }
     }
 }
